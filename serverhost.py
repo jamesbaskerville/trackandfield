906c6882a96dec137daad866cc6a4fea59d857c8
James Baskerville <baskerville@college.harvard.edu>
2015-06-18 22:08:18 -0700

    Awgrjn

--- serverhost.py
+++ serverhost.py
@@ -1,6 +1,8 @@
 import smtplib
-import SMTP
+
 server = smtplib.SMTP("smtp.gmail.com",587)
 server.starttls()
 server.login( 'kumabaskerice@gmail.com', #password )
 
+
+server.sendmail('Your Team', numbers, message)
--- smtp.py
+++
@@ -1,4 +0,0 @@
-
-def sendText (numbers, message):
-    for i in range len(numbers):
-        server.sendmail('Your Team', numbers[i], message)
